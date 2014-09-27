a4df82c2b83b7e23524f111a86078721bfd57643
Dylan Hutchison <dhutchis@stevens.edu>
2014-09-27 01:38:50 -0400

    Rename, add travis CI, add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Testing Accumulo iterators.
+Also testing [Travis CI](https://travis-ci.org/).
+
+[![Build Status](https://travis-ci.org/Stevens-GraphGroup/accumuloiter.svg)](https://travis-ci.org/Stevens-GraphGroup/accumuloiter)
+
--- src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
+++ src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.stevens.dhutchis.accumuloitertest;
+package edu.stevens.dhutchis.accumuloiter;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
