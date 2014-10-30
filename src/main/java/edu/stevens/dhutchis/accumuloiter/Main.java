52770a3af89f9556784bf11715e02287fd6b1d3e
Dylan Hutchison <dhutchis@stevens.edu>
2014-10-29 20:14:22 -0400

    Add skeleton for new combiner; mess with IntelliJ stuff

--- src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
+++ src/main/java/edu/stevens/dhutchis/accumuloiter/Main.java
@@ -1,44 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package edu.stevens.dhutchis.accumuloiter;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import org.apache.accumulo.core.client.AccumuloException;
-import org.apache.accumulo.core.client.AccumuloSecurityException;
-import org.apache.accumulo.core.client.BatchWriter;
-import org.apache.accumulo.core.client.BatchWriterConfig;
-import org.apache.accumulo.core.client.Connector;
-import org.apache.accumulo.core.client.Instance;
-import org.apache.accumulo.core.client.IteratorSetting;
+import org.apache.accumulo.core.client.*;
 import org.apache.accumulo.core.client.Scanner;
-import org.apache.accumulo.core.client.TableExistsException;
-import org.apache.accumulo.core.client.TableNotFoundException;
-import org.apache.accumulo.core.client.mock.MockInstance;
-import org.apache.accumulo.core.client.security.tokens.PasswordToken;
-import org.apache.accumulo.core.data.ByteSequence;
 import org.apache.accumulo.core.data.Key;
 import org.apache.accumulo.core.data.Mutation;
-import org.apache.accumulo.core.data.Range;
 import org.apache.accumulo.core.data.Value;
 import org.apache.accumulo.core.iterators.Combiner;
-import org.apache.accumulo.core.iterators.IteratorEnvironment;
 import org.apache.accumulo.core.iterators.IteratorUtil;
 import org.apache.accumulo.core.iterators.LongCombiner;
-import org.apache.accumulo.core.iterators.SortedKeyValueIterator;
 import org.apache.accumulo.core.iterators.user.SummingCombiner;
 import org.apache.accumulo.core.security.Authorizations;
 import org.apache.hadoop.io.Text;
 import org.junit.Assert;
-import org.junit.Test;
+
+import java.util.*;
+import java.util.Map.Entry;
 
 /**
  *
@@ -94,67 +71,28 @@ public class Main {
         BatchWriter writer = conn.createBatchWriter(tableName, config);
         writer.addMutation(m1);
         writer.flush();
-
-
         
         // check results
         Scanner scan = conn.createScanner(tableName, Authorizations.EMPTY);
         System.out.println("Scanner range: "+scan.getRange());
         for(Entry<Key,Value> entry : scan) {
-//            Text row = entry.getKey().getRow();
-//            Value value = entry.getValue();
             System.out.println(entry);
             Assert.assertEquals("12", entry.getValue().toString());
-            // hmm, MockAccumulo does not seem to use iterators and prints 5,
-            // MiniAccumuloCluster correctly prints 12.
         }
+
+        // try a custom iterator...
         
     }
     
-    class NewIter implements SortedKeyValueIterator<Key,Value> 
-    {
-
+    class ConcatIter extends Combiner {
         @Override
-        public void init(SortedKeyValueIterator<Key, Value> source, Map<String, String> options, IteratorEnvironment env) throws IOException {
-            env.registerSideChannel(null); // what does this do?
-            
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        public Value reduce(Key key, Iterator<Value> iter) {
+            String ss = "";
+            while (iter.hasNext()) {
+                Value v = iter.next();
+                ss += v.toString();
+            }
+            return new Value(ss.getBytes());
         }
-
-        @Override
-        public boolean hasTop() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void next() throws IOException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public void seek(Range range, Collection<ByteSequence> columnFamilies, boolean inclusive) throws IOException {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Key getTopKey() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public Value getTopValue() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-
-        @Override
-        public SortedKeyValueIterator<Key, Value> deepCopy(IteratorEnvironment env) {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        }
-        
-    }
-    
-    public static void main(String[] args) throws AccumuloException, AccumuloSecurityException, TableNotFoundException, TableExistsException {
-        //new Main().testIter();
-        
     }
 }
